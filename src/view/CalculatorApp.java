d8725a6362edd7f52f43c9fb68fbe3a24df3565e
gr40in <Gr40in@yandex.ru>
2023-06-30 13:49:03 +0500

    Initial commit

---
+++ src/view/CalculatorApp.java
@@ -0,0 +1,28 @@
+package view;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+public class CalculatorApp extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("ComplexCalcFullView.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+//        String css = this.getClass().getResource("darkStyle.css").toExternalForm();
+//        scene.getStylesheets().add(0,"darkStyle.css");
+        stage.setScene(scene);
+        stage.setTitle("Complex Calculator");
+        scene.setFill(Color.TRANSPARENT);
+        stage.initStyle(StageStyle.TRANSPARENT);
+        stage.setResizable(false);
+        stage.show();
+    }
+
+    public void run(){
+        launch();
+    }
+}
